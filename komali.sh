98391ac9c26d7ededa6135befd4969816288d209
kayamvasu <kayamvasuachari1@gmail.com>
2023-03-15 17:12:34 +0530

    komali.sh

--- komali.sh
+++ komali.sh
@@ -1,3 +1,13 @@
+<<<<<<< Updated upstream
+<<<<<<< Updated upstream
 
 just going  to delete this file in a few secs , after you pull the code to you loacal repo
 just updating 
+=======
+just updating
+i'm in master branch
+>>>>>>> Stashed changes
+=======
+just updating
+i'm in master branch
+>>>>>>> Stashed changes
